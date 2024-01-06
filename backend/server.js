06d9c8ae04ca0c0877b218685379af88842fa161
LeeYooBin <santosroberdan25@gmail.com>
2024-01-06 12:34:48 -0300

    Setting initial configs

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connection = require("./src/database/database");
+
+const app = express();
+connection();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors(
+  {
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
+  }
+));
+
+app.listen(8080, () => {
+  console.log("Server running on http://localhost:8080");
+});
